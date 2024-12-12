761c4f3ddafc19606dc981d0ded30bc08a272049
minhtri2k3 <118947935+minhtri2k3@users.noreply.github.com>
2024-12-12 20:44:22 +0700

    Update
    Modify detail screen

--- app/src/main/java/com/example/pokemonproject/screen/PokemonScreen/PokemonDetailScreen/PokemonDetailScreen.kt
+++ app/src/main/java/com/example/pokemonproject/screen/PokemonScreen/PokemonDetailScreen/PokemonDetailScreen.kt
@@ -1,11 +1,15 @@
 import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,16 +17,23 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
@@ -34,11 +45,14 @@ import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import coil.compose.AsyncImage
 import com.example.myapplication.R
+import com.example.myapplication.ui.theme.AguDisplay
 import com.example.myapplication.ui.theme.elementColor
+import com.example.myapplication.ui.theme.elementIcon
 import com.example.pokemonproject.data.network.DTO.PokemonDTO
 import com.example.pokemonproject.domain.model.PokemonState
 import com.example.pokemonproject.domain.model.PokemonStatus
 import com.example.pokemonproject.screen.PokemonScreen.PokemonDetailScreen.PokemonScreenViewModel
+import kotlinx.coroutines.Delay
 
 
 @Composable
@@ -46,7 +60,7 @@ fun PokemonDetailScreen(
     viewModel: PokemonScreenViewModel = hiltViewModel(),
     id: Int,
     animatedVisibilityScope: AnimatedVisibilityScope,
-    pokemonImageSize: Dp = 200.dp
+    pokemonImageSize: Dp = 300.dp
 ) {
     LaunchedEffect(key1 = true) {
         viewModel.fetchPokemon(id)
@@ -112,11 +126,18 @@ fun PokemonDetailScreen(
                             verticalAlignment = Alignment.CenterVertically,
                             modifier = Modifier.fillMaxWidth() // Optional: Make the row take full width
                         ){
-                            Box(
-                                modifier = Modifier
+                            Text(
+                                text = pokemon.name.replaceFirstChar { it.uppercaseChar() },
+                                style = TextStyle(
+                                    color = Color(0xFFC9B710),
+                                    fontSize = 35.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    fontFamily = AguDisplay,
+                                ),
+                                modifier = Modifier.padding(bottom = 8.dp)
                             )
                             Image(
-                                painter = painterResource(id = R.drawable.fire),
+                                painter = painterResource(id = elementIcon(pokemon.types[0])),
                                 contentDescription = "Fire Icon",
                                 modifier = Modifier
                                     .size(75.dp) // Adjust size as needed
@@ -129,16 +150,43 @@ fun PokemonDetailScreen(
                             modifier = Modifier
                                 .size(pokemonImageSize)
                         )
-                        Text(
-                            text = pokemon.name.replaceFirstChar { it.uppercaseChar() },
-                            style = TextStyle(
-                                color = Color.White,
-                                fontSize = 20.sp,
-                                fontWeight = FontWeight.Bold,
-                            ),
-                            modifier = Modifier.padding(bottom = 8.dp)
-                        )
                         PokemonTypesRow(types = pokemon.types)
+                        PokemonStat(
+                            statName = "HP",
+                            statValue = pokemon.hp.toInt(),
+                            statMaxValue = 100,
+                            statColor = Color.Green,
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        PokemonStat(
+                            statName = "ATK",
+                            statValue = pokemon.attack.toInt(),
+                            statMaxValue = 100,
+                            statColor = Color.Red,
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        PokemonStat(
+                            statName = "DEF",
+                            statValue = pokemon.defense.toInt(),
+                            statMaxValue = 100,
+                            statColor = Color(0xFF18aded),
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        PokemonStat(
+                            statName = "SpAtk",
+                            statValue = pokemon.special_attack.toInt(),
+                            statMaxValue = 100,
+                            statColor = Color(0xFF9512db),
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+                        PokemonStat(
+                            statName = "SpDef",
+                            statValue = pokemon.special_defense.toInt(),
+                            statMaxValue = 100,
+                            statColor = Color(0xFFd90cf0),
+                        )
+                        Spacer(modifier = Modifier.height(10.dp))
+
                     }
                 }
 
@@ -154,7 +202,7 @@ fun PokemonTypesRow(types: List<String>) {
     Row(
         horizontalArrangement = Arrangement.spacedBy(8.dp), // Space between items
         verticalAlignment = Alignment.CenterVertically,
-        modifier = Modifier.padding(16.dp) // Padding around the Row
+        modifier = Modifier.padding(16.dp) // Padding    around the Row
     ) {
         types.forEach { type ->
             Text(
@@ -173,3 +221,65 @@ fun PokemonTypesRow(types: List<String>) {
         }
     }
 }
+
+
+@Composable
+fun PokemonStat(
+    statName: String,
+    statValue: Int,
+    statMaxValue: Int,
+    statColor: Color,
+    height: Dp = 28.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercent = animateFloatAsState(
+        targetValue = if(animationPlayed) {
+            statValue / statMaxValue.toFloat()
+        } else 0f,
+        animationSpec = tween(
+            animDuration,
+            animDelay
+        )
+    )
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(height)
+            .clip(CircleShape)
+            .background(
+                if (isSystemInDarkTheme()) {
+                    Color(0xFF505050)
+                } else {
+                    Color.LightGray
+                }
+            )
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier
+                .fillMaxHeight()
+                .fillMaxWidth(curPercent.value)
+                .clip(CircleShape)
+                .background(statColor)
+                .padding(horizontal = 8.dp)
+        ) {
+            Text(
+                text = statName,
+                fontWeight = FontWeight.Bold
+            )
+            Text(
+                text = (curPercent.value * statMaxValue).toInt().toString(),
+                fontWeight = FontWeight.Bold
+            )
+        }
+    }
+}
+
